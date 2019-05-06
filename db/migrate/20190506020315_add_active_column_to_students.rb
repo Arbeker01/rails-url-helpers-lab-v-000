13213459cc279919a1eaabf1686f51e26683180f
Romeo Gono <romeogono1@aol.com>
2019-05-06 02:05:03 +0000

    Automatically backed up by Learn

--- db/migrate/20190506020315_add_active_column_to_students.rb
+++ db/migrate/20190506020315_add_active_column_to_students.rb
@@ -1,6 +1,5 @@
 class AddActiveColumnToStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :string
-    add_column :students, :boolean, :string
+    add_column :students, :boolean, 
   end
 end
